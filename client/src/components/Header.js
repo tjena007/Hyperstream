b7cfde3b31fa7c6fb9afb48aa8b02e97fabc4e10
Tanmay Jena <tjena2@gmail.com>
2022-01-04 19:00:45 -0800

    added functionality to display streams list
    associated user id to each stream

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,7 +5,7 @@ import GoogleAuth from './GoogleAuth'
 const Header = () => {
     return (
         <div className="ui secondary pointing menu">
-        <Link to='/' className="item">Streamy</Link>
+        <Link to='/' className="item">Hyperstream</Link>
          <div className="right menu">
              <Link to="/" className="item">All streams</Link>
          </div>
